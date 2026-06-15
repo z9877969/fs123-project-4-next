958d312ea990a2d54a5af49aaaffea617bc71d06
Andrii Khomenko <akhomenko1995@gmail.com>
2026-06-15 19:57:02 +0200

    add fonts and global css variables

--- app/(private routes)/recipes/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function RecipesPage() {
-  redirect("/recipes/filter/all");
-}
--- app/api/_utils/utils.ts
+++
@@ -1,5 +0,0 @@
-export function logErrorResponse(error: unknown) {
-  if (process.env.NODE_ENV !== "production") {
-    console.error("API error:", error);
-  }
-}
